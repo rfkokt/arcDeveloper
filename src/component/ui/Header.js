e0df04cbed3f94e99f7b314b57a253aa22e10bdb
rifki okta <rifkiokta105@gmail.com>
2021-06-08 16:26:39 +0700

    update header

--- src/component/ui/Header.js
+++ src/component/ui/Header.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import {AppBar, Toolbar, useScrollTrigger, Tabs, Tab, Button} from "@material-ui/core";
+import {AppBar, Toolbar, useScrollTrigger, Tabs, Tab, Button, Menu, MenuItem} from "@material-ui/core";
 import {makeStyles} from "@material-ui/styles";
 import {Link} from "react-router-dom";
 
@@ -53,15 +53,64 @@ const useStyles = makeStyles(theme => ({
         marginRight: '25px',
         height: '45px',
     },
+    menu: {
+        backgroundColor: theme.palette.common.blue,
+        color: "white",
+        borderRadius: '0px',
+        //    ^membuat kotak menu item tidak ada rouded
+    },
+    menuItem: {
+        ...theme.typography.tab,
+        opacity: 0.7,
+        //    ^membuat ukuran text menuItem sama besar dengan ukuran tabs
+        "&:hover": {
+            opacity: 1
+        }
+    }
 }))
 
 export default function Header(props) {
     const classes = useStyles();
     const [value, setValue] = useState(0)
+    const [anchorEl, setAnchorEl] = useState(null)
+    const [open, setOpen] = useState(false)
+    const [selectedIndex, setSelectedIndex] = useState(0)
 
     const handleChange = (e, value) => {
         setValue(value)
     }
+
+    const handleClick = (e) => {
+        setAnchorEl(e.currentTarget)
+        setOpen(true)
+    }
+
+    const handleClose = (e) => {
+        setAnchorEl(null)
+        setOpen(false)
+    }
+
+    const handleMenuItemClick = (e, i) => {
+        setAnchorEl(null)
+        setOpen(false)
+        setSelectedIndex(i)
+    }
+
+    const menuOptions = [
+        {
+            name: "Services",
+            link: "/services"
+        }, {
+            name: "Custom Software Development",
+            link: "/customsoftware"
+        }, {
+            name: "Mobile App Development",
+            link: "/mobileapps"
+        }, {
+            name: "Website Development",
+            link: "/websites"
+        },
+    ]
 
     useEffect(() => {
         //Digunakan untuk menjaga value dan url tetap sama ketika page di refresh.
@@ -75,6 +124,61 @@ export default function Header(props) {
             setValue(3)
         } else if (window.location.pathname === "/contact" && value !== 4) {
             setValue(4)
+        }
+
+        switch (window.location.pathname) {
+            //^ untuk mengatuk ketika sedang berada di suatu url, active tab akan mengikutin url yg tersedia
+            case "/":
+                if (value !== 0) {
+                    setValue(0)
+                }
+                break;
+            case "/services":
+                if (value !== 1) {
+                    setValue(1)
+                    setSelectedIndex(0)
+                }
+                break;
+            case "/customsoftware":
+                if (value !== 1) {
+                    setValue(1)
+                    setSelectedIndex(1)
+                }
+                break;
+            case "/mobileapps":
+                if (value !== 1) {
+                    setValue(1)
+                    setSelectedIndex(2)
+                }
+                break;
+            case "/websites":
+                if (value !== 1) {
+                    setValue(1)
+                    setSelectedIndex(3)
+                }
+                break;
+            case "/revolution" :
+                if (value !== 2) {
+                    setValue(2)
+                }
+                break;
+            case "/about" :
+                if (value !== 3) {
+                    setValue(3)
+                }
+                break;
+            case "/contact" :
+                if (value !== 4) {
+                    setValue(4)
+                }
+                break;
+            case "/estimate" :
+                if (value !== 5) {
+                    setValue(5)
+                }
+                break;
+            default:
+                break;
         }
     }, [value])
     return (
@@ -97,9 +201,13 @@ export default function Header(props) {
                                  component={Link}
                                  to={"/"}
                                  label={"Home"}/>
-                            <Tab className={classes.tab}
-                                 to={"/services"}
-                                 component={Link} label={"Services"}/>
+                            <Tab
+                                aria-owns={anchorEl ? "simple-menu" : undefined}
+                                aria-haspopup={anchorEl ? "true" : undefined}
+                                onMouseOver={(e) => handleClick(e)}
+                                className={classes.tab}
+                                to={"/services"}
+                                component={Link} label={"Services"}/>
                             <Tab className={classes.tab}
                                  to={"/revolution"}
                                  component={Link} label={"The Revolution"}/>
@@ -113,6 +221,31 @@ export default function Header(props) {
                         <Button variant={"contained"} color={'secondary'} className={classes.button}>
                             Free Estimate
                         </Button>
+                        <Menu id={"simple-menu"} anchorEl={anchorEl} open={open}
+                              onClose={handleClose}
+                              MenuListProps={{onMouseLeave: handleClose}}
+                              onClick={() => {
+                                  handleClose();
+                                  setValue(1)
+                              }}
+                              classes={{paper: classes.menu}}
+                              elevation={0}
+                        >
+
+                            {menuOptions.map((option, i) => (
+                                <MenuItem key={option} onClick={(e) => {
+                                    handleMenuItemClick(e, i);
+                                    setValue(1)
+                                }}
+                                          selected={i === selectedIndex && value === 1}
+                                          component={Link} to={option.link}
+                                          classes={{root: classes.menuItem}}
+                                >{option.name}
+                                </MenuItem>
+                            ))}
+                            {/*^ terpilih ketika value nya 1 atau berada di tabs services ,
+                            selected={i === selectedIndex && value === 1}*/}
+                        </Menu>
                     </Toolbar>
                 </AppBar>
             </ElevationScroll>
