02965aa698648387a1ca5f47658e0a3c2b7cdb0a
rifki okta <rifkiokta105@gmail.com>
2021-06-10 17:15:29 +0700

    update & menyederhanakan code

--- src/component/ui/Header.js
+++ src/component/ui/Header.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import {
     AppBar, Toolbar, useScrollTrigger, Tabs, Tab, Button, Menu, MenuItem, useMediaQuery,
-    SwipeableDrawer, Icon, IconButton
+    SwipeableDrawer, IconButton, List, ListItem, ListItemText
 } from "@material-ui/core";
 import {makeStyles, useTheme} from "@material-ui/styles";
 import MenuIcon from '@material-ui/icons/Menu';
@@ -95,6 +95,26 @@ const useStyles = makeStyles(theme => ({
     drawerIcon: {
         height: "50px",
         width: "50px"
+    },
+    drawer: {
+        backgroundColor: theme.palette.common.blue
+    },
+    drawerItem: {
+        ...theme.typography.tab,
+        color: 'white',
+        opacity: 0.7
+    },
+    drawerItemEstimate: {
+        backgroundColor: theme.palette.common.orange
+    },
+    drawerItemSelected: {
+        ...theme.typography.tab,
+        "& .MuiListItemText-root": {
+            opacity: 1,
+        },
+    },
+    appBar: {
+        zIndex: theme.zIndex.modal + 1
     }
 }))
 
@@ -133,88 +153,75 @@ export default function Header(props) {
     const menuOptions = [
         {
             name: "Services",
-            link: "/services"
+            link: "/services",
+            activeIndex: 1,
+            selectedIndex: 0
         }, {
             name: "Custom Software Development",
-            link: "/customsoftware"
+            link: "/customsoftware",
+            activeIndex: 2,
+            selectedIndex: 1
         }, {
             name: "Mobile App Development",
-            link: "/mobileapps"
+            link: "/mobileapps",
+            activeIndex: 3,
+            selectedIndex: 2
         }, {
             name: "Website Development",
-            link: "/websites"
+            link: "/websites",
+            activeIndex: 4,
+            selectedIndex: 3
         },
+    ]
+
+    const routes = [
+        {
+            name: "Home",
+            link: "/",
+            activeIndex: 0,
+        },
+        {
+            name: "Services",
+            link: "/service",
+            activeIndex: 1,
+            ariaOwns: anchorEl ? "simple-menu" : undefined,
+            ariaPopup: anchorEl ? "true" : undefined,
+            mouseOver: (e) => handleClick(e)
+        },
+        {
+            name: "The Revolution",
+            link: "/revolution",
+            activeIndex: 2,
+        },
+        {
+            name: "About Us",
+            link: "/about",
+            activeIndex: 3,
+        },
+        {
+            name: "Contact Us",
+            link: "/contact",
+            activeIndex: 4,
+        }
     ]
 
     useEffect(() => {
         //Digunakan untuk menjaga value dan url tetap sama ketika page di refresh.
-        if (window.location.pathname === "/" && value !== 0) {
-            setValue(0)
-        } else if (window.location.pathname === "/services" && value !== 1) {
-            setValue(1)
-        } else if (window.location.pathname === "/revolution" && value !== 2) {
-            setValue(2)
-        } else if (window.location.pathname === "/about" && value !== 3) {
-            setValue(3)
-        } else if (window.location.pathname === "/contact" && value !== 4) {
-            setValue(4)
-        }
-
-        switch (window.location.pathname) {
-            //^ untuk mengatur ketika sedang berada di suatu url, active tab akan mengikutin url yg tersedia
-            case "/":
-                if (value !== 0) {
-                    setValue(0)
-                }
-                break;
-            case "/services":
-                if (value !== 1) {
-                    setValue(1)
-                    setSelectedIndex(0)
-                }
-                break;
-            case "/customsoftware":
-                if (value !== 1) {
-                    setValue(1)
-                    setSelectedIndex(1)
-                }
-                break;
-            case "/mobileapps":
-                if (value !== 1) {
-                    setValue(1)
-                    setSelectedIndex(2)
-                }
-                break;
-            case "/websites":
-                if (value !== 1) {
-                    setValue(1)
-                    setSelectedIndex(3)
-                }
-                break;
-            case "/revolution" :
-                if (value !== 2) {
-                    setValue(2)
-                }
-                break;
-            case "/about" :
-                if (value !== 3) {
-                    setValue(3)
-                }
-                break;
-            case "/contact" :
-                if (value !== 4) {
-                    setValue(4)
-                }
-                break;
-            case "/estimate" :
-                if (value !== 5) {
-                    setValue(5)
-                }
-                break;
-            default:
-                break;
-        }
-    }, [value])
+        [...menuOptions, ...routes].forEach(route => {
+            switch (window.location.pathname) {
+                case `${route.link}`:
+                    if (value !== route.activeIndex) {
+                        setValue(route.activeIndex)
+                        if (route.selectedIndex && route.selectedIndex !== selectedIndex) {
+                            setSelectedIndex(route.selectedIndex)
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+        })
+    }, [value, menuOptions, selectedIndex, routes])
 
     const tabs = (
         //ditampilkan ketika layar berukuran >= medium const matches = useMediaQuery(theme.breakpoints.down("md"))
@@ -226,26 +233,18 @@ export default function Header(props) {
                 indicatorColor={'primary'}
             >
                 {/* indicatorColor => untuk menghilangkan garis bawah di tab dengan mengganti warna sama menjadi warna primary*/}
-                <Tab className={classes.tab}
-                     component={Link}
-                     to={"/"}
-                     label={"Home"}/>
-                <Tab
-                    aria-owns={anchorEl ? "simple-menu" : undefined}
-                    aria-haspopup={anchorEl ? "true" : undefined}
-                    onMouseOver={(e) => handleClick(e)}
-                    className={classes.tab}
-                    to={"/services"}
-                    component={Link} label={"Services"}/>
-                <Tab className={classes.tab}
-                     to={"/revolution"}
-                     component={Link} label={"The Revolution"}/>
-                <Tab className={classes.tab}
-                     to={"/about"}
-                     component={Link} label={"About Us"}/>
-                <Tab className={classes.tab}
-                     to={"/contact"}
-                     component={Link} label={"Contact Us"}/>
+                {routes.map((route, index) => (
+                    <Tab
+                        key={`${route}${index}`}
+                        className={classes.tab}
+                        component={Link}
+                        to={route.link}
+                        label={route.name}
+                        aria-haspopup={route?.ariaPopup}
+                        aria-owns={route?.ariaOwns}
+                        onMouseOver={route.mouseOver}
+                    />
+                ))}
             </Tabs>
             <Button variant={"contained"} color={'secondary'} className={classes.button}>
                 Free Estimate
@@ -257,16 +256,18 @@ export default function Header(props) {
                       handleClose();
                       setValue(1)
                   }}
+                  style={{zIndex: 1302}}
                   classes={{paper: classes.menu}}
                   elevation={0}
+                  keepMounted
             >
 
                 {menuOptions.map((option, i) => (
-                    <MenuItem key={option} onClick={(e) => {
+                    <MenuItem key={`${option}${i}`} onClick={(e) => {
                         handleMenuItemClick(e, i);
-                        setValue(1)
+                        setValue(option.selectedIndex)
                     }}
-                              selected={i === selectedIndex && value === 1}
+                              selected={i === selectedIndex}
                               component={Link} to={option.link}
                               classes={{root: classes.menuItem}}
                     >{option.name}
@@ -281,9 +282,46 @@ export default function Header(props) {
     const drawer = (
         //ditampilkan ketika layar berukuran < medium
         <>
-            <SwipeableDrawer anchor={"right"} disableBackdropTransition={!iOS} disableDiscovery={iOS} open={openDrawer}
-                             onClose={() => setOpenDrawer(false)} onOpen={() => setOpenDrawer(true)}>
-                Example Drawer
+            <SwipeableDrawer anchor={"right"} disableBackdropTransition={!iOS}
+                             disableDiscovery={iOS}
+                             open={openDrawer}
+                             onClose={() => setOpenDrawer(false)}
+                             onOpen={() => setOpenDrawer(true)}
+                             classes={{paper: classes.drawer}}
+            >
+                <div className={classes.toolbarMargin}/>
+                {/*^ digunakan untuk meletakan drawer di bawah header */}
+                <List disablePadding>
+                    {routes.map((route, index) => (
+                        <ListItem
+                            key={`${route}${index}`}
+                            onClick={() => {
+                                setOpenDrawer(false);
+                                setValue(route.activeIndex)
+                            }}
+                            divider button component={Link}
+                            to={route.link}
+                            selected={value === route.activeIndex}
+                            classes={{selected: classes.drawerItemSelected}}
+                        >
+                            <ListItemText disableTypography
+                                          className={classes.drawerItem}>
+                                {route.name}
+                            </ListItemText>
+                        </ListItem>
+                    ))}
+                    <ListItem classes={{root: classes.drawerItemEstimate, selected: classes.drawerItemSelected}}
+                              onClick={() => {
+                                  setOpenDrawer(false);
+                                  setValue(5)
+                              }} divider button
+                              component={Link} to={"/estimate"}
+                              selected={value === 5}
+                    >
+                        <ListItemText className={classes.drawerItem}
+                                      disableTypography>Free Estimate</ListItemText>
+                    </ListItem>
+                </List>
             </SwipeableDrawer>
             <IconButton className={classes.drawerIconContainer} onClick={() => setOpenDrawer(!openDrawer)}
                         disableRipple>
@@ -294,7 +332,7 @@ export default function Header(props) {
     return (
         <React.Fragment>
             <ElevationScroll>
-                <AppBar position={"fixed"}>
+                <AppBar position={"fixed"} className={classes.appBar}>
                     <Toolbar disableGutters>
                         <Button component={Link} disableRipple to={"/"} onClick={() => setValue(0)}
                                 className={classes.logoContainer}>
